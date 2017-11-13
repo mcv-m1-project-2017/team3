3a42adbff4d322b05c68cb536e2683a6fb7612c9
rogermm14 <rogermarimolas@gmail.com>
2017-11-13 02:01:53 +0100

    Add files via upload

---
+++ week5/Compute_Mask_Functions/scg_segmentation.m
@@ -0,0 +1,95 @@
+function [final_mask] = mcg_segmentation(im, features, seg_method)
+
+final_mask = zeros(size(im,1),size(im,2)); % init. final mask
+
+[candidates_mcg, ucm2_mcg] = im2mcg(im,'fast'); % segment image
+
+% we decide that only those 'objects' with score higher than 0.3 are relevant
+[candidates_mcg.scores,idx] = sort(candidates_mcg.scores,'descend');
+candidates_mcg.scores = candidates_mcg.scores(candidates_mcg.scores > 0.25);
+
+% load model histograms for histogram comparison
+load('hue_sig.mat');
+
+for i = 1:size(candidates_mcg.scores,1) 
+    
+    discard = 0; % set to 1 if this component is to be discarded
+    
+    mask = ismember(candidates_mcg.superpixels, candidates_mcg.labels{idx(i)});
+    bbox = candidates_mcg.bboxes(idx(i),:);
+    roi_mask = mask(bbox(1):bbox(3),bbox(2):bbox(4));
+    roi_im = im(bbox(1):bbox(3),bbox(2):bbox(4),:);
+    roi_mask = imfill(roi_mask,'holes');
+    
+    % extract features of current candidate
+    width = bbox(4)-bbox(2);
+    height = bbox(3)-bbox(1);
+    bbox_area = width*height;
+    form_factor = width/height;
+    signal_size = sum(sum(roi_mask));
+    %imshow(roi_mask);
+    
+    % filter according to form_factor, size and bbox area
+    if ~(    ( form_factor>= 0.5 && form_factor<= 1.2 )  ...
+          && ( signal_size >= min(features.size.min) && signal_size <= max(features.size.max)) ...
+          && ( bbox_area >= 900 && bbox_area <= 56000 ) )
+                discard = 1;
+    end
+    
+    % filter according to histogram comparison (Hue channel)
+    edges = linspace(0,1,101);
+    if (discard == 0)
+        candidate_HSV = rgb2hsv(roi_im);
+        candidate_H = candidate_HSV(:,:,1);
+        hist_candidate = histcounts(candidate_H(roi_mask~=0),edges)/numel(candidate_H);
+        match_values_H = zeros([1 6]);
+        for h=1:6
+            match_values_H(h) = sum(min(hist_candidate, hue_sig{h}));
+        end
+        %imshow(roi_im.*uint8(roi_mask));
+        %match_values_H
+        if match_values_H < 0.4
+            discard = 1;
+        else
+            [maxim, index] = max(match_values_H);
+            if (index == 1 || index == 2 || index == 3) ... 
+               && abs(maxim-match_values_H(4)) < 0.2 ...
+               && abs(maxim-match_values_H(6)) < 0.2
+                discard = 1;
+            elseif (index == 4 || index == 6 ) && maxim < 0.5
+                discard = 1;
+            end
+        end
+    end
+    
+    % the component is kept if it has not been discarded at this point
+    if discard == 0
+        final_mask(bbox(1):bbox(3),bbox(2):bbox(4)) = roi_mask;
+    end
+    
+end
+
+%final_mask = imclose(final_mask, strel('disk',9));
+final_mask = imfill(final_mask);
+
+%subplot(2,3,1), imshow(im), subplot(2,3,2), imshow(final_mask);
+%subplot(2,3,3), imshow(imdilate(ucm2_mcg,strel(ones(3))));
+
+switch(seg_method)
+    case 'SCG_TM'
+        % run template matching (distance transform) as implemented in week 4
+        if exist('Templates/templates_edges.mat')
+            load ('Templates/templates_edges.mat')
+        else
+            get_templates_predefined;
+            load ('Templates/templates_edges.mat')
+        end
+        final_mask = template_matching_DT(final_mask, templates_edges);
+    case 'SCG_Hough'  
+        %Hough transform goes here
+    otherwise
+        
+end
+%subplot(2,3,4), imshow(final_mask);
+
+end
---
+++ week5/hough_filter.m
@@ -0,0 +1,52 @@
+function [absolve] = hough_filter (candidate)
+% addpath('Compute_Mask_Functions\CircularHough_Grd');
+dev_thr=5; %threshold in angle degrees
+
+absolve=false;
+candidate_edg=edge(candidate);
+%     candidate_edg=edge(candidate,'canny',[0.05, 0.2],1);
+
+[H,theta,rho] = hough(candidate_edg);
+peaks = houghpeaks(H,4);
+lines = houghlines(candidate_edg,theta,rho,peaks);
+
+horizontal_line = [];
+vertical_line = [];
+obl_line = [];
+ for n = 1:length(lines)
+     %detect lines orientations
+     if((abs(lines(n).theta) >= 90 - dev_thr) && (abs(lines(n).theta) <= 90 + dev_thr))
+         horizontal_line = [horizontal_line; [lines(n).point1 lines(n).point2]]; %Horizontal line
+
+     elseif((abs(lines(n).theta) >= 0 - dev_thr) && (abs(lines(n).theta) <= 0 + dev_thr))
+         vertical_line = [vertical_line; [lines(n).point1, lines(n).point2]];%Vertical line
+
+     elseif((abs(lines(n).theta) >= 30 - dev_thr) && (abs(lines(n).theta) <= 30 + dev_thr))
+         obl_line = [obl_line; [lines(n).point1, lines(n).point2, lines(n).theta]];%oblique
+
+     end
+ end
+
+ hor_num = size(horizontal_line,1);
+ vert_num = size(vertical_line,1);
+ triang_num = size(obl_line,1);
+
+ %square?
+ if ((hor_num==2)&&(vert_num==2))
+     absolve=true;
+ end
+ %triangle?
+ if ((hor_num>=1)&&(vert_num>=2))
+     absolve=true;
+ end
+ %circle?
+ maximum_radius=min(length(candidate(:,1)),length(candidate(1,:)));
+ minimum_radius=maximum_radius/2;
+ radrange=[minimum_radius,maximum_radius];
+%      [~, center] = CircularHough_Grd(double(candidate_edg), radrange);
+ [center,rad] = imfindcircles(double(candidate_edg), radrange);
+ if (~isempty(center))
+    absolve=true; 
+ end
+     
+end
